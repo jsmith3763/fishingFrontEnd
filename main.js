d07541d44c511e8fd29e0a47a610a121d588a79d
Justin Smith <jsmith3763@gmail.com>
2022-04-27 12:38:34 -0700

    Pulling data from API and displaying fish names

--- main.js
+++ main.js
@@ -2,9 +2,30 @@
 const $body = $("body");
 const $headerDiv = $('<div class = "header"></div>');
 $headerDiv.appendTo($body);
-const $welcomeHeader = $('<h1>Welcome to Fishy.com</>');
+const $welcomeHeader = $('<h1>Welcome To Fish List</>');
 $welcomeHeader.appendTo($headerDiv);
+const $page = $('<div id="page"></div>');
+$page.appendTo($body);
+const $fishContainer = $('<div id="fishContainer"></div>');
+$fishContainer.appendTo($page);
+$leftRow = $(`<div id="leftRow"></div>`);
+$rightRow = $(`<div id="rightRow"></div>`);
+$leftRow.appendTo($fishContainer);
+$rightRow.appendTo($fishContainer);
 
-$.get('https://www.fishwatch.gov/api/species')
+let counter = 0;
 
-//test
+$.get("https://www.fishwatch.gov/api/species", (data) => {
+    for(let i = 0; i < data.length; i++) {
+        const $fishDiv = $("<div class='fish-card'></div>");
+        if(counter % 2 === 0) {
+            $fishDiv.appendTo($leftRow);
+        }else {
+            $fishDiv.appendTo($rightRow);
+        }
+        counter++;
+        const $h5 = $(`<h5 class="fish">${data[i]["Species Name"]}</h3>`)
+        $h5.appendTo($fishDiv);
+    }
+});
+
