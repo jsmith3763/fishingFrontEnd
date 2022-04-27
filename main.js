80f9c23102cfd8b44b719ea8ecc5f3b4833aada4
jsmith3763 <57576309+jsmith3763@users.noreply.github.com>
2022-04-27 09:58:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//creating home page structure
+const $body = $("body");
+const $headerDiv = $('<div class = "header"></div>');
+$headerDiv.appendTo($body);
+const $welcomeHeader = $('<h1>Welcome to Fishy.com</>');
+$welcomeHeader.appendTo($headerDiv);
+
+$.get('https://www.fishwatch.gov/api/species')
