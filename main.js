fd1a899ffe41ab650bc859f9033dcb0ffcf4bb06
Justin Smith <jsmith3763@gmail.com>
2022-04-27 14:55:16 -0700

    Added Links and started UserList Function

--- main.js
+++ main.js
@@ -13,9 +13,24 @@ $rightRow = $(`<div id="rightRow"></div>`);
 $leftRow.appendTo($fishContainer);
 $rightRow.appendTo($fishContainer);
 
+
+
+//creating home button
+const $homeButton = $('<button class = "homeButton"></button>');
+$homeButton.text("Home");
+$homeButton.appendTo($headerDiv);
+$homeButton.hide();
+$($homeButton).on('click', function(e) {
+  //unhide all of the other information
+    $($fishContainer).show();
+    $($welcomeHeader).show();
+    $('.homeButton').hide();
+})
+
 let counter = 0;
 
 $.get("https://www.fishwatch.gov/api/species", (data) => {
+    console.log(data);
     for(let i = 0; i < data.length; i++) {
         const $fishDiv = $("<div class='fish-card'></div>");
         if(counter % 2 === 0) {
@@ -26,6 +41,20 @@ $.get("https://www.fishwatch.gov/api/species", (data) => {
         counter++;
         const $h5 = $(`<h5 class="fish">${data[i]["Species Name"]}</h3>`)
         $h5.appendTo($fishDiv);
+        const $image = $(`<img class="card-image" src=${data[i]["Species Illustration Photo"].src}></ul>`);
+        $image.appendTo($fishDiv);
+        const $a = $(`<a id='learn-more' href='${`https://www.fishwatch.gov` + data[i].Path}'>Learn More</a>`)
+        $a.appendTo($fishDiv);
+        $($fishDiv).on('click', function(e) {
+            //unhide all of the other information
+            userList();
+        })
     }
 });
+
+function userList() {
+    $($fishContainer).hide();
+    $($welcomeHeader).hide();
+    $('.homeButton').show();
+}
 
