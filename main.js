9b6302a2f96d6f3775dff22b19ba2a14e0abd48c
Justin Smith <jsmith3763@gmail.com>
2022-04-28 08:50:16 -0700

    Added input for caught fish attributes

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 //creating home page structure
 const $body = $("body");
-const $headerDiv = $('<div class = "header"></div>');
-$headerDiv.appendTo($body);
-const $welcomeHeader = $('<h1>Welcome To Fish List</>');
-$welcomeHeader.appendTo($headerDiv);
-const $page = $('<div id="page"></div>');
-$page.appendTo($body);
-const $fishContainer = $('<div id="fishContainer"></div>');
-$fishContainer.appendTo($page);
-$leftRow = $(`<div id="leftRow"></div>`);
-$rightRow = $(`<div id="rightRow"></div>`);
-$leftRow.appendTo($fishContainer);
-$rightRow.appendTo($fishContainer);
-
+const $headerDiv = $('<div class = "header"></div>').appendTo($body);;
+const $welcomeHeader = $('<h1>Welcome To Fish List</>').appendTo($headerDiv);
+const $page = $('<div id="page"></div>').appendTo($body);;
+const $fishContainer = $('<div id="fishContainer"></div>').appendTo($page);;
+const $leftRow = $(`<div id="leftRow"></div>`).appendTo($fishContainer);;
+const $rightRow = $(`<div id="rightRow"></div>`).appendTo($fishContainer);;
+const $listHeader = $(`<h1 id='list-header'>Fish Caught</h1>`).appendTo($headerDiv);
+const $userList = $(`<div class="userList"></div>`).appendTo($page);;
+$($listHeader).hide();
+$($userList).hide();
 
 
 //creating home button
-const $homeButton = $('<button class = "homeButton"></button>');
+const $homeButton = $('<button class="homeButton"></button>');
 $homeButton.text("Home");
 $homeButton.appendTo($headerDiv);
 $homeButton.hide();
 $($homeButton).on('click', function(e) {
   //unhide all of the other information
-    $($fishContainer).show();
-    $($welcomeHeader).show();
-    $('.homeButton').hide();
+    hideUserList();
 })
 
-let counter = 0;
+//creating view list button
+const $viewList = $('<button class="viewList"></button>');
+$viewList.text("View List");
+$viewList.appendTo($headerDiv);
+$($viewList).on('click', function(e) {
+  //unhide all of the other information
+    hideHomeScreen();
+})
 
+//determines what row to place fish card
+let counter = 0;
+//gets info from fishwatch API and displays on page
 $.get("https://www.fishwatch.gov/api/species", (data) => {
     console.log(data);
     for(let i = 0; i < data.length; i++) {
@@ -43,18 +48,54 @@ $.get("https://www.fishwatch.gov/api/species", (data) => {
         $h5.appendTo($fishDiv);
         const $image = $(`<img class="card-image" src=${data[i]["Species Illustration Photo"].src}></ul>`);
         $image.appendTo($fishDiv);
-        const $a = $(`<a id='learn-more' href='${`https://www.fishwatch.gov` + data[i].Path}'>Learn More</a>`)
+        const $a = $(`<a class='learn-more' href='${`https://www.fishwatch.gov` + data[i].Path}'>Learn More</a>`)
         $a.appendTo($fishDiv);
-        $($fishDiv).on('click', function(e) {
+        const $addButton = $('<button class="add"></button>').text("Fish Caught").appendTo($fishDiv);
+        $($addButton).on('click', function(e) {
             //unhide all of the other information
-            userList();
+            //hideHomeScreen();
+            addToList($fishDiv);
         })
     }
 });
 
-function userList() {
+//hides and shows certain elements
+function hideHomeScreen() {
     $($fishContainer).hide();
     $($welcomeHeader).hide();
+    $($viewList).hide();
+    $(".add").hide();
+    $(".learn-more").hide();
     $('.homeButton').show();
+    $($listHeader).show();
+    $($userList).show();
+}
+
+//hides userList screen
+function hideUserList() {
+    $($fishContainer).show();
+    $($welcomeHeader).show();
+    $($viewList).show();
+    $(".add").show();
+    $(".learn-more").show();
+    $('.homeButton').hide();
+    $($listHeader).hide();
+    $($userList).hide();
+}
+
+//adds clicked div to userList
+function addToList(element) {
+    const $currentElement = element.clone();
+    $currentElement.attr('id', 'userlist-div');
+    $currentElement.appendTo($userList);
+    const weight = window.prompt('Enter weight of fish:');
+    const length = window.prompt('Enter length:');
+    const location = window.prompt('Where did you catch it?');
+    const method = window.prompt("What method was used?");
+    $ul = $(`<ul class="unordered-list" style="list-style-type:none; margin: 0; padding: 0; text-align: center;"></ul>`).appendTo($currentElement);
+    $li = $(`<li class="fish-info">Weight: ${weight}</ul>`).appendTo($ul);
+    $li = $(`<li class="fish-info">Length: ${length}</ul>`).appendTo($ul);
+    $li = $(`<li class="fish-info">Location: ${location}</ul>`).appendTo($ul);
+    $li = $(`<li class="fish-info">How was it caught?: ${method}</ul>`).appendTo($ul);
 }
 
